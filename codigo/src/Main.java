fffedfb77c94c77746d9a6d17794faaa3c8cdd96
braga-arthur <arthesbraga@gmail.com>
2022-04-24 23:22:34 -0300

    fourth commit

--- codigo/src/Main.java
+++ codigo/src/Main.java
@@ -1,7 +1,24 @@
+import java.io.*;
+
 public class Main {
+    private static String lerArquivo(String path) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader(path));
+        String arquivo = "";
+        String linha = "";
 
-    public static void main(String[] args) {
+        while(linha != null) {
+            linha = br.readLine();
+            if(linha != null) arquivo += (linha + "\n");
+        }
 
+        br.close();
+
+        return arquivo;
+    }
+
+    public static void main(String[] args) throws Exception {
+        
+        System.out.println( lerArquivo("in.txt") );
         //passar arquivo fazer leitura e enviar pedido
     }
 }
